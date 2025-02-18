b761a5f45db493071fb60dddb913c2772bb7fdd4
Vaishnavi Sharma <115636677+Vaishnavi26082005@users.noreply.github.com>
2025-02-19 00:20:00 +0530

    Time: 7 ms (13.61%), Space: 41.9 MB (25.41%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -3,28 +3,27 @@ class Solution {
       return cs(pattern); 
     }
     public static String cs(String s){
-        int n= s.length();
-        int ans[] =new int[n+1];
-        Stack<Integer>st= new Stack<>();
-        int c=1;
-        for(int i=0;i<=n;i++){
-            if(i==n||s.charAt(i)=='I'){
-              ans[i]=c;
-              c++;
-              while(!st.isEmpty()){
-                int x=st.pop();
-               ans[x]=c;
+       int c=1;
+       int arr[]= new int [s.length()+1];
+       Stack<Integer>st= new Stack<>();
+       for (int i=0;i<=s.length();i++){
+        if(i==s.length()||s.charAt(i)=='I'){
+            arr[i]=c;
+            c++;
+            while(!st.isEmpty()){
+                int a= st.pop();
+                arr[a]=c;
                 c++;
-              }
-            }
-            else{
-                st.push(i);
             }
         }
-        String str1="";
-     for(int i=0;i<n+1;i++){
-        str1+=ans[i];
-     }
-     return str1;
+        else{
+            st.push(i);
+        }
+       }
+       String str="";
+       for(int i=0;i<arr.length;i++){
+        str+=arr[i];
+       }
+       return str;
     }
 }
