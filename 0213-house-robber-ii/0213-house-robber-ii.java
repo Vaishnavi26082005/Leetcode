a200e946bb5bc8defdcf3b90291a723bf03eda09
Vaishnavi Sharma <115636677+Vaishnavi26082005@users.noreply.github.com>
2025-03-11 22:11:45 +0530

    Time: 0 ms (100%), Space: 41.2 MB (49.04%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,43 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-         if(nums.length==1){
+        if (nums.length == 1) {
             return nums[0];
         }
-        
-        
-        int n=nums.length;
-        
-        int j=0;
-        int k=0;
-       int num1[]=new int[n-1];
-       int num2[]=new int[n-1];
-       for(int i=0;i<nums.length;i++){
-        if(i!=0){
-            num1[j]=nums[i];
-            j++;
-
-        }
-        if(i!=n-1){
-            num2[k]=nums[i];
-            k++;
-        }
-       } 
-       return Math.max(rob1(num1),rob1(num2));
+        return Math.max(rob1(nums, 0, nums.length - 2), rob1(nums, 1, nums.length - 1));
     }
-    public int rob1(int[] nums){
-        if(nums.length==1){
-            return nums[0];
-        }
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
-        int prev1=Math.max(nums[0],nums[1]);
 
-        int prev2=nums[0];
-        int ans=0;
-        for(int i=2;i<nums.length;i++){
-            ans=Math.max(nums[i]+prev2,prev1);
-            prev2=prev1;
-            prev1=ans;
+    private int rob1(int[] nums, int start, int end) {
+        int prev2 = 0, prev1 = 0;
+        int curr=0;
+        for (int i = start; i <= end; i++) {
+            curr = Math.max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return ans;
+        return curr;
     }
 }
