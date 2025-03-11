605d5648e6630896b351b5083f4dcd9dcc34708b
Vaishnavi Sharma <115636677+Vaishnavi26082005@users.noreply.github.com>
2025-03-11 12:30:19 +0530

    Time: 2 ms (94.85%), Space: 46.8 MB (48.48%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] countBits(int n) {
         int arr[]=new int[n+1];
-        for(int i=0;i<=n;i++){
+        for(int i=1;i<=n;i++){
             arr[i]=arr[i>>1]+(i&1);
         }
         return arr;
