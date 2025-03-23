1329ae8546d6cc7596e64a7a1d47330d48108742
Vaishnavi Sharma <115636677+Vaishnavi26082005@users.noreply.github.com>
2025-03-23 16:52:04 +0530

    Time: 1 ms (93.54%), Space: 43.3 MB (11.53%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+      List<List<Integer>>ans= new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<TreeNode>qq= new LinkedList<>();
+        qq.offer(root);
+        while(!qq.isEmpty()){
+            List<Integer>l= new ArrayList<>();
+            int s=qq.size();
+            while(s!=0){
+            TreeNode r=qq.poll();
+            l.add(r.val);
+            if(r.left!=null){
+                qq.add(r.left);
+            }
+            if(r.right!=null){
+                qq.add(r.right);
+            }
+         s--;
+            }
+       
+            ans.add(l);
+
+
+        }
+        Collections.reverse(ans);
+        return ans;  
+    }
+}
