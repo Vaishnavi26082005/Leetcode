1ef7884763a72571340b15f6bc6ca32f8133f9cb
Vaishnavi Sharma <115636677+Vaishnavi26082005@users.noreply.github.com>
2025-04-02 22:55:29 +0530

    Time: 1 ms (99.31%), Space: 41.8 MB (86.6%) - LeetHub

---
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        int n=nums.length;
+        int left[]= new int [n];
+        int right[]= new int[n];
+       
+       
+        for(int i=1;i<n;i++){
+            left[i]=Math.max(left[i-1],nums[i-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            right[i]=Math.max(right[i+1],nums[i+1]);
+        }
+        long max= 0;
+        for(int i=1;i<n;i++){
+            max=Math.max(max,(long)(left[i]-nums[i])*right[i]);
+        }
+        return max;
+        
+    }
+}
