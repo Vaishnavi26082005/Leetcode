708e9f4e61b590a69b3d43537674fb0de92db7b8
Vaishnavi Sharma <115636677+Vaishnavi26082005@users.noreply.github.com>
2025-07-07 15:05:30 +0530

    Time: 8 ms (96.14%), Space: 48.9 MB (42.91%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,47 +1,45 @@
 class Solution {
-    public int[] dijakstra(ArrayList<ArrayList<int[]>>adj,int src,int n){
-        PriorityQueue<int[]>pq=new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[]p1,int[]p2){
+    public int networkDelayTime(int[][] times, int n, int k) {
+        ArrayList<ArrayList<int[]>>adj= new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int [] a:times){
+            int u=a[0];
+            int v=a[1];
+            int c=a[2];
+            adj.get(u).add(new int[]{v,c});
+        }
+        int dist[]= new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k]=0;
+        PriorityQueue<int[]>pq= new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int[]p1,int [] p2){
                 return p1[1]-p2[1];
             }
         });
-        int times[]=new int[n];
-         Arrays.fill(times,Integer.MAX_VALUE);
-         times[src]=0;
-         pq.offer(new int[]{src,0});
-         while(!pq.isEmpty()){
-            int pair[]=pq.poll();
-            int u=pair[0];
-            int d=pair[1];
-            if(d>times[u])continue;
-            for(int arr[]:adj.get(u)){
-                int v=arr[0];
-                int w=arr[1];
-                if(times[u]+w<times[v]){
-                    times[v]=times[u]+w;
-                    pq.offer(new int[]{v,times[v]});
-                }
-            }
-         }return times;
-    } 
-    public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<ArrayList<int[]>>adj= new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int arr[]:times){
-            int u=arr[0]-1;
-            int v=arr[1]-1;
-            int w=arr[2];
-            adj.get(u).add(new int[]{v,w});
-        }
-        int[] brr=dijakstra( adj,k-1,n);
-        int res=Integer.MIN_VALUE;
-       for(int i = 0; i < n; i++) {
-    if(brr[i] == Integer.MAX_VALUE) return -1;
-    res = Math.max(brr[i], res);
-}
+        pq.add(new int[]{k,0});
+        while(!pq.isEmpty()){
+            int[] curr= pq.poll();
+            int node=curr[0];
+            int d=curr[1];
+            if(d>dist[node])continue;
 
-return res;
+            for(int[] nbr:adj.get(node)){
+               int v=nbr[0];
+               int c=nbr[1];
+               if(d+c<dist[v]){
+                dist[v]=d+c;
+                pq.offer(new int[]{v,dist[v]});
+               }
+            }
+        }
+        int max=Integer.MIN_VALUE;
+
+        for(int i=1;i<=n;i++){
+           max=Math.max(max,dist[i]);      
+        }
+        return max==Integer.MAX_VALUE?-1:max;
     }
 }
