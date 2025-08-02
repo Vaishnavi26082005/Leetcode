83858004de6cd9185098c4e01c52a8edb056b8f0
Vaishnavi Sharma <115636677+Vaishnavi26082005@users.noreply.github.com>
2025-08-03 03:10:33 +0530

    Time: 19 ms (60.81%), Space: 51.4 MB (30.39%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -6,10 +6,7 @@ class Solution {
         for(int i=0;i<=n;i++){
             dp[i][0]=1;
         }
-        for(int j=1;j<=m;j++){
-            dp[0][j]=0;
-        }
-
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
