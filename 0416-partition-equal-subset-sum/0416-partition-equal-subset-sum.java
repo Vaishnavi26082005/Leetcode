a2f132584611a4ba5af4deae23424cec7355ca6d
Vaishnavi Sharma <115636677+Vaishnavi26082005@users.noreply.github.com>
2025-08-31 20:22:46 +0530

    Time: 45 ms (60.04%), Space: 47.5 MB (50.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -3,29 +3,27 @@ class Solution {
         int tar=0;
         for(int i=0;i<nums.length;i++){
             tar+=nums[i];
-
         }
-        if(tar%2==1)return false;
+        if(tar%2==1)  return false;
         tar=tar/2;
-        int n=nums.length;
-        boolean dp[][]=new boolean[n][tar+1];
-
+        int n= nums.length;
+        boolean dp[][]=new boolean [n][tar+1];
         for(int i=0;i<n;i++){
             dp[i][0]=true;
         }
-          if(nums[0]<=tar)
+        if(nums[0]<=tar)
         dp[0][nums[0]]=true;
         for(int i=1;i<n;i++){
             for(int j=0;j<=tar;j++){
-                boolean pick=false;
-                if(j>nums[i]){
-                  pick= dp[i-1][j-nums[i]];
+                boolean pic=false;
+                if(j>=nums[i]){
+                    pic=dp[i-1][j-nums[i]];
                 }
-                boolean not_pick=dp[i-1][j];
-                dp[i][j]=pick||not_pick;
+                boolean np=dp[i-1][j];
+                dp[i][j]=pic||np;
             }
         }
-
-return dp[n-1][tar];
+        return dp[n-1][tar];
     }
+
 }
