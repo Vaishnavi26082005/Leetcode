83905f8544a1eb85bf8ad8d6777d765eb4ec4a6e
Vaishnavi Sharma <vaishnavicool1234@gmail.com>
2025-09-16 23:37:51 +0530

    Time: 0 ms (100%), Space: 41.8 MB (5.69%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,18 +1,21 @@
 class Solution {
-    public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
+    public int rob(int[] arr) {
+        int n=arr.length;
+
+        if (arr.length == 1) {
+            return arr[0];
         }
-        return Math.max(rob1(nums, 0, nums.length - 2), rob1(nums, 1, nums.length - 1));
+        return Math.max(find(arr,0,arr.length-2),find(arr,1,arr.length-1));
     }
 
-    private int rob1(int[] nums, int start, int end) {
-        int prev2 = 0, prev1 = 0;
+    public int find(int arr[],int start,int end){
+        int prev1=0;
+        int prev2=0;
         int curr=0;
-        for (int i = start; i <= end; i++) {
-            curr = Math.max(nums[i] + prev2, prev1);
-            prev2 = prev1;
-            prev1 = curr;
+        for(int i=start;i<=end;i++){
+          curr=Math.max(prev2+arr[i],prev1);
+          prev2=prev1;
+          prev1=curr;
         }
         return curr;
     }
