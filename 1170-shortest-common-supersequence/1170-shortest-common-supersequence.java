925bd262dbe9ad7e6a3519f626be01ac198800d1
Vaishnavi Sharma <vaishnavicool1234@gmail.com>
2025-09-17 11:41:02 +0530

    Time: 21 ms (79.07%), Space: 50.8 MB (20.07%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,46 +1,46 @@
 class Solution {
-    public String shortestCommonSupersequence(String s1, String s2) {
-        int n=s1.length();
-        int m=s2.length();
+    public String shortestCommonSupersequence(String s, String t) {
+        int n=s.length();
+        int m=t.length();
         int dp[][]= new int[n+1][m+1];
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-               if(s1.charAt(i-1)==s2.charAt(j-1)){
-                dp[i][j]=1+dp[i-1][j-1];
-               }
-               else{
-                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-               }
+                  if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                  }
+                  else {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                  }
             }
         }
-  StringBuilder sb= new StringBuilder();
-  int i=n;
-  int j=m;
-  int len=dp[n][m];
-  
-  while(i>0&&j>0){
-  if(s1.charAt(i-1)==s2.charAt(j-1)){
-    sb.insert(0,s1.charAt(i-1));
-    i--;
-    j--;
-  }
-  else if(dp[i-1][j]>dp[i][j-1]){
-    sb.insert(0,s1.charAt(i-1));
-    i--;
-  }
-  else{
-    sb.insert(0,s2.charAt(j-1));
-    j--;
-  }
-  }
-  while(i>0){
-    sb.insert(0,s1.charAt(i-1));
-    i--;
-  }
-  while(j>0){
-    sb.insert(0,s2.charAt(j-1));
-    j--;
-  }
-  return sb.toString();
+        int i=n;
+        int j=m;
+        int len=dp[n][m]; 
+        StringBuilder sb= new StringBuilder();
+
+        while(i>0&&j>0){
+            if(s.charAt(i-1)==t.charAt(j-1)){
+                sb.insert(0,s.charAt(i-1));
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
+                sb.insert(0,s.charAt(i-1));
+                i--;
+            }
+            else{
+                sb.insert(0,t.charAt(j-1));
+                j--;
+            }
+        }
+        while(i>0){
+            sb.insert(0,s.charAt(i-1));
+                i--;
+        }
+        while(j>0){
+            sb.insert(0,t.charAt(j-1));
+                j--;
+        }
+        return sb.toString();
     }
 }
