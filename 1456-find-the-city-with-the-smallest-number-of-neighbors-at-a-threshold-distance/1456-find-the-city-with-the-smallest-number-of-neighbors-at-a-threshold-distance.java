ced30f3f598dbb430a3427f937a36ca4cee486c8
Vaishnavi Sharma <vaishnavicool1234@gmail.com>
2025-09-19 04:55:04 +0530

    Time: 8 ms (91.29%), Space: 44.6 MB (45.22%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,44 +1,41 @@
 class Solution {
-    public int findTheCity(int n, int[][] edges, int z) {
-        int[][] dist= new int [n][n];
-        for(int a[]:dist)Arrays.fill(a,1000004);
-        for (int i = 0; i < n; i++) {
-            dist[i][i] = 0;
+    public int findTheCity(int n, int[][] edges, int t) {
+        int[][] mat = new int[n][n];
+        
+        for(int a[]:mat){
+            Arrays.fill(a,Integer.MAX_VALUE/2);
         }
-
+        for(int i=0;i<n;i++){
+            mat[i][i]=0;
+        }
         for(int a[]:edges){
             int u=a[0];
             int v=a[1];
-            int c=a[2];
-            dist[u][v]=c;
-            dist[v][u]=c;
+            int w=a[2];
+            mat [u][v]=w;
+            mat[v][u]=w;
+
         }
 
         for(int k=0;k<n;k++){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-              dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    mat[i][j]=Math.min(mat[i][j],mat[i][k]+mat[k][j]);
+                }
             }
         }
-    }
-    int min=n;
-    int cnt=0;
-    int city=-1;
-
-    for(int i=0;i<n;i++){
-        cnt=0;
+  int nas=Integer.MAX_VALUE;
+  int city=0;
+     for(int i=0;i<n;i++){
+        int cnt=0;
         for(int j=0;j<n;j++){
-            if(dist[i][j]<=z){
-                cnt++;
-            }
+            if(mat[i][j]<=t)cnt++;
         }
-        if(cnt<=min){
-            min=cnt;
-            city=i;
+        if(nas>=cnt){
+          nas=cnt;
+          city =i;
         }
-       
+     }
+     return city;
     }
-   return city;
-}
-  
 }
